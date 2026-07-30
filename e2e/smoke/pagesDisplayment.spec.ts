0d2088951ba7ab455af990181f1672bee1832bed
PPat93 <45639693+PPat93@users.noreply.github.com>
2026-07-30 13:14:42 +0100

    vP.0 - a bit of rearrangements and preparations for the next part

--- e2e/smoke/pagesDisplayment.spec.ts
+++ e2e/smoke/pagesDisplayment.spec.ts
@@ -1,21 +1,43 @@
-import {test, expect} from '@playwright/test';
+import {test, expect} from "@playwright/test";
 import {TEXTS} from "../shared/shared";
 import {LoginPage, loginPageTexts} from "../pages/loginPage";
 
+test.describe(`Login Page displayment`, async () => {
 
-test('Login page is displayed', async ({page}) => {
+    test(`Login page is displayed`, async ({page}) => {
 
-    //  Arrange
-    const loginPage = new LoginPage(page);
+        //  Arrange
+        const loginPage = new LoginPage(page);
 
-    //  Act
-    await page.goto('/login');
+        //  Act
+        await page.goto(`/login`);
 
-    //  Asserts
-    await expect(page).toHaveTitle(TEXTS.appName);
-    await expect(loginPage.pageTitle).toHaveText(TEXTS.appName);
-    await expect(loginPage.pageSubTitle).toHaveText(TEXTS.subtitle);
-    await expect(loginPage.logo).toBeVisible();
-    await expect(loginPage.passwordField).toBeVisible();
-    await expect(loginPage.submitBtn).toHaveText(loginPageTexts.SUBMIT_BTN_TXT);
+        //  Asserts
+        await expect(page).toHaveTitle(TEXTS.appName);
+        await expect(loginPage.pageTitle).toHaveText(TEXTS.appName);
+        await expect(loginPage.pageSubTitle).toHaveText(TEXTS.subtitle);
+        await expect(loginPage.logo).toBeVisible();
+        await expect(loginPage.passwordField).toBeVisible();
+        await expect(loginPage.submitBtn).toHaveText(loginPageTexts.SUBMIT_BTN_TXT);
+    })
+})
+
+test.describe(`Main page displayments`, async () => {
+
+    test(`Stock page is displayed`, async ({page}) => {
+
+        //  Arrange
+        const loginPage = new LoginPage(page);
+
+        //  Act
+        await page.goto(`/login`);
+
+        //  Asserts
+        await expect(page).toHaveTitle(TEXTS.appName);
+        await expect(loginPage.pageTitle).toHaveText(TEXTS.appName);
+        await expect(loginPage.pageSubTitle).toHaveText(TEXTS.subtitle);
+        await expect(loginPage.logo).toBeVisible();
+        await expect(loginPage.passwordField).toBeVisible();
+        await expect(loginPage.submitBtn).toHaveText(loginPageTexts.SUBMIT_BTN_TXT);
+    })
 })
