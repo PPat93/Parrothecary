506cb2fd820938f00535d4f1b393f9a081c11a9b
PPat93 <45639693+PPat93@users.noreply.github.com>
2026-08-29 21:25:36 +0100

    vP.3 - add global db setup

--- e2e/shared/global.db.prep.ts
+++ e2e/shared/global.db.prep.ts
@@ -1,8 +1,38 @@
+import {execSync} from "node:child_process";
+import fs from "node:fs";
+import path from "node:path";
+
 const smoke = process.env.DATABASE_PATH_SMOKE;
 const functional = process.env.DATABASE_PATH_FUNC;
 
-export default function guard(name: string, value: string | undefined): string {
+function guard(value: string | undefined): string {
     if (!value) throw new Error(`DB path value not set up`);
     if (!value.includes(`DBs`)) throw new Error(`DB from fixtures should be selected`);
     return value;
+}
+
+function run(command: string, dbPath: string) {
+    execSync(command, {
+        stdio: 'inherit',
+        env: {...process.env, DATABASE_PATH: dbPath},
+    })
+}
+
+export default async function globalSetup() {
+    const smokeDb = guard(smoke);
+    const functDb = guard(functional);
+    const dbSuffixes = [`-wal`, `-shm`];
+
+    for (const singleDB of [smokeDb, functDb]) {
+        fs.mkdirSync(path.dirname(singleDB), {recursive: true});
+
+        for (const suffix of dbSuffixes) {
+            fs.rmSync(singleDB + suffix, {force: true});
+        }
+    }
+
+    run(`npm run db:reset -- --force`, smokeDb);
+
+    run(`npm run db:reset -- --force`, functDb);
+    run(`npm run db:seed`, functDb);
 }
--- playwright.config.ts
+++ playwright.config.ts
@@ -58,6 +58,7 @@ export default defineConfig({
     workers: process.env.CI ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: 'html',
+    globalSetup: './e2e/shared/global.db.prep',
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('')`. */
